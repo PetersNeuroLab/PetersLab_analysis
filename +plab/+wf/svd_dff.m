d2d3633d677df9ea5add93dc0dd55f04778756ca
Andy Peters <peters.andrew.j@gmail.com>
2023-08-14 12:02:02 +0100

    update

--- +plab/+wf/svd_dff.m
+++ +plab/+wf/svd_dff.m
@@ -1,11 +1,11 @@
-function Vdf = svd_dff(U, V, meanImage)
+function Vdf = svd_dff(U, V, mean_image)
 % Vdf = svd_dff(U, V, meanImage)
 % (from cortexlab)
 
 % Get the mean image in U-space (meanV)
 [nX, nY, nSVD] = size(U);
 flatU = reshape(U, nX*nY,nSVD);
-meanV = flatU'*meanImage(:);
+meanV = flatU'*mean_image(:);
 
 % Define the baseline V as the reconstructed mean (meanV) + mean activity
 V0 = meanV + mean(V,2);
@@ -14,8 +14,8 @@ V0 = meanV + mean(V,2);
 Vdf= bsxfun(@minus,V,mean(V,2));
 
 % Get (soft) dF/F in U-space by dividing U by average + soft
-df_softnorm = median(meanImage(:))*1;
-nonnormU = reshape(bsxfun(@rdivide,flatU,reshape(meanImage,[],1)+df_softnorm), [nX nY nSVD]);
+df_softnorm = median(mean_image(:))*1;
+nonnormU = reshape(bsxfun(@rdivide,flatU,reshape(mean_image,[],1)+df_softnorm), [nX nY nSVD]);
 
 % New df/f U's aren't orthonormal: change df/f V basis set into old U space
 Vdf = plab.wf.change_U(nonnormU,Vdf,U);
