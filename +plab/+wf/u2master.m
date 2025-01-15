95faae69f56810c5a3facf0dda9d6dece3fe4f4c
Andy Peters <peters.andrew.j@gmail.com>
2025-01-15 16:54:06 +0000

    Update u2master.m

--- +plab/+wf/u2master.m
+++ +plab/+wf/u2master.m
@@ -5,7 +5,7 @@ function [U_master,V_master] = u2master(U,V)
 % (loads U_master, changes basis for V's)
 
 % Load master U
-plab.wf.load_master_U;
+U_master = plab.wf.load_master_U;
 
 % Change V basis set from U to U_master
 V_master = plab.wf.change_U(U,V,U_master);
